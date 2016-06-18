023d29324c7c34d2e7706174570405a3a914fa89
John <John@192.168.1.100>
2016-06-18 17:46:39 +0200

    new Date Model and ordering of list in updateGUI at end

--- src/HealthObservationExaminationThree/Observations.java
+++ src/HealthObservationExaminationThree/Observations.java
@@ -2,6 +2,7 @@
  */
 package HealthObservationExaminationThree;
 
+import java.util.Comparator;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -9,7 +10,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- * A wrapper class for the List of root element Observation
+ * A wrapper class for the List of root element Observation.
  * allows better operation of JAXB library to save XML text file of data
  * @author John McInnes
  */
@@ -39,5 +40,57 @@ public class Observations
     public void setObservations(List<Observation> observations) 
     {
         this.observations = observations;
+    }
+    /**
+     * Returns the average temperature of all observations
+     */
+    public Double getObservationsAverageTemp() 
+    {
+        if(!observations.isEmpty())
+        {
+            Double averageTemp = 0.0;
+            int numObser = 0;
+            for (Object loopObject : observations)
+            {
+                Observation currentObservation = (Observation)loopObject;          
+                averageTemp =+currentObservation.getEntryTemp();
+                numObser++;       
+            }
+            return averageTemp = averageTemp/numObser;
+        }
+        return 0.0;
+    }
+    /**
+     * Returns the highest temperature of all observations
+     */
+    public Double getObservationsHighestTemp() 
+    {
+        class temperatureCompare implements Comparator<Observation>
+        {
+            public int compare(Observation a, Observation b)
+            {
+                    return (a.getEntryTemp().compareTo(b.getEntryTemp()));
+            }
+        
+        }
+        if(!observations.isEmpty())
+        {
+//            Double highestTemp = 0.0;
+            
+                    observations.sort(new temperatureCompare());
+                     Double highestTemp = observations.get(0).getEntryTemp();
+                     System.out.println("highest temp" +highestTemp);
+                     return highestTemp;
+//            int numObser = 0;
+//            
+//            for (Object loopObject : observations)
+//            {
+//                Observation currentObservation = (Observation)loopObject;          
+//                highestTemp =+currentObservation.getEntryTemp();
+//                numObser++;       
+//            }
+//            return averageTemp = averageTemp/numObser;
+        }
+        return 0.0;
     }
 }
